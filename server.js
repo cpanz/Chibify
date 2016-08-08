67dfca7dcade8f9a6ea35a486f1808fc994bda95
Cheng Pan <cproripop@gmail.com>
2016-08-08 01:15:13 -0400

    Fix port

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ catch (e) {
 const mongoUri = process.env.MONGODB_URI || config.MONGODB_URI;
 mongoose.connect(mongoUri, options);
 
-
-app.listen(3000, function() {
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
   console.log('Listening on port 3000');
 });
 
